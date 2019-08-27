150ee6f7c449ed2c725c2b64a0c1ab9e23b569cf
Nazim Uddin <mdnazimuddincse@gmail.com>
2019-08-27 06:05:52 +0600

    Create a new Project

---
+++ CustomCollectionView/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCollectionView
+//
+//  Created by Nazim Uddin on 19/8/19.
+//  Copyright © 2019 Nazim Uddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
